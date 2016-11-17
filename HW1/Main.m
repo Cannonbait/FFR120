177e0041a9ae369c8f2d3929edd39e605487d60d
Cannonbait <ivar_busig@hotmail.com>
2016-11-17 23:20:09 +0100

    Removed asv files

--- HW1/Main.m
+++ HW1/Main.m
@@ -5,43 +5,41 @@ RATIO = [0.99; 0.01; 0];
 INITIAL_SUSCEPTIBLE = TOTAL_AGENTS*RATIO(1);
 INITIAL_INFECTED = TOTAL_AGENTS*RATIO(2);
 INITIAL_RESISTANT = TOTAL_AGENTS*RATIO(3);
-INFECTION_RATE = 0.2;
-RECOVERY_RATE = 0.002:0.002:0.02;
+INFECTION_RATE = 0.1:0.1:1;
 MOVEMENT_RATE = 0.3;     % Refered to as d
 NUM_TRIALS = 10;
-
+RATIO_STEPS = 1:10:151;
 
 % scatter(agents(:,1), agents(:,2));
 
-statistics = zeros(size(RECOVERY_RATE,2), 1);
+statistics = zeros(size(INFECTION_RATE,2), size(RATIO_STEPS,2));
 
 % latticeFigure = figure();
 % PlotLattice(latticeFigure, agents);
+tic;
 
-parfor iRecoveryRate = 1:size(RECOVERY_RATE,2)
-  for iTrial = 1:NUM_TRIALS
+for iBeta = 1:size(INFECTION_RATE,2)
+  infectionRate = INFECTION_RATE(iBeta);
+  recoveryRate = infectionRate ./ RATIO_STEPS;
+  for iRecoveryRate = 1:size(recoveryRate,2)
     average = 0;
-    agents = InitializeAgents(AREA_SIZE, INITIAL_SUSCEPTIBLE, INITIAL_INFECTED, INITIAL_RESISTANT);
-    iTimestep = 1;
-    while sum(sum(agents(:,:,2))) > 0 && iTimestep <= 5000;
-      % Movement
-      agents = MoveAgents(agents, AREA_SIZE, MOVEMENT_RATE);
+    for iTrial = 1:NUM_TRIALS
+      agents = InitializeAgents(AREA_SIZE, INITIAL_SUSCEPTIBLE, INITIAL_INFECTED, INITIAL_RESISTANT);
+      iTimestep = 1;
+      while sum(sum(agents(:,:,2))) > 0 && iTimestep <= 5000;
+        % Movement
+        agents = MoveAgents(agents, AREA_SIZE, MOVEMENT_RATE);
 
-      %Disease update
-      agents = UpdateDisease(agents, AREA_SIZE, INFECTION_RATE, RECOVERY_RATE(iRecoveryRate));
+        %Disease update
+        agents = UpdateDisease(agents, AREA_SIZE, infectionRate, recoveryRate(iRecoveryRate));
 
-    % %   PlotLattice(latticeFigure, agents);
-    %   countSusceptible = sum(sum(agents(:,:,1)));
-    %   countInfected = sum(sum(agents(:,:,2)));
-    %   countResistant = sum(sum(agents(:,:,3)));
-    %   
-    %   statistics(iTimestep,:) = [countSusceptible countInfected, countResistant];
-      iTimestep = iTimestep + 1;
+        iTimestep = iTimestep + 1;
+      end
+      average = average + sum(sum(agents(:,:,3)));
     end
-    average = average + sum(sum(agents(:,:,3)));
+    statistics(iBeta,iRecoveryRate) = average/NUM_TRIALS;
   end
-  statistics(iRecoveryRate) = average/NUM_TRIALS;
 end
 % statistics = statistics(1:iTimestep-1,:);
 statistics
-
+toc;
