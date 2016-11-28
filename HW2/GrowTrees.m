be7e548e69246b9791d3367bf6302627abef453b
Cannonbait <ivar_busig@hotmail.com>
2016-11-28 16:29:50 +0100

    GrowTrees written

--- HW2/GrowTrees.m
+++ HW2/GrowTrees.m
@@ -1,5 +1,7 @@
 function area = GrowTrees( area, GROWTH_RATE)
-  
+  newTrees = rand(size(area));
+  newTrees = arrayfun(@(x) floor(x+GROWTH_RATE), newTrees);
+  area = arrayfun(@(x) min(1,x), newTrees+area);
 
 end
 
