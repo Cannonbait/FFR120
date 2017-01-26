bb482afd3856181343ad56fd1a14d71ae61d0270
Cannonbait <ivar_busig@hotmail.com>
2017-01-26 21:34:42 +0100

    Implementation work on Task 6

--- HW4/CalculateClusteringCoefficient.m
+++ HW4/CalculateClusteringCoefficient.m
@@ -1,7 +1,4 @@
 function coefficient = CalculateClusteringCoefficient( graph )
-  
-
-
   numTriangles = trace(graph^3)/2;
   numVertices = arrayfun(@(x) x*(x-1)/2, sum(graph));
   numVertices = sum(numVertices);
--- HW4/Task6.m
+++ HW4/Task6.m
@@ -1,3 +1,35 @@
 close all;
 clear;
 clc;
+
+Network1;
+Network2;
+Network3;
+
+graph1 = ListToGraph(network1, max(network1));
+graph2 = ListToGraph(network2, max(network2));
+graph3 = ListToGraph(network3, max(network3));
+
+VisualizeGraph(graph1);
+VisualizeGraph(graph2);
+VisualizeGraph(graph3);
+% 
+% CalculateClusteringCoefficient(graph1)
+% CalculateClusteringCoefficient(graph2)
+% CalculateClusteringCoefficient(graph3)
+
+
+
+% distributions1 = CalculateDegreeDistribution(graph1);
+% range1 = (1:max(network1));
+% 
+% distributions2 = CalculateDegreeDistribution(graph2);
+% range2 = (1:max(network2));
+% 
+% distributions3 = CalculateDegreeDistribution(graph3);
+% range3 = (1:max(network3));
+% loglog(range1, distributions1, range2, distributions2, range3, distributions3);
+% 
+CalculatePathStatistics(graph1)
+CalculatePathStatistics(graph2)
+CalculatePathStatistics(graph3)
